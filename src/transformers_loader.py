88ddd2d64aa658fc8a61f1c46458a14cb8096cf9
nrozen <nrozen@redhat.com>
2024-06-25 06:39:48 +0300

    added ignore for transformers mypy type check

--- src/transformers_loader.py
+++ src/transformers_loader.py
@@ -1,5 +1,5 @@
 import torch
-from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers import AutoModelForCausalLM, AutoTokenizer  # type: ignore
 from src.model_loader import BaseModelLoader
 
 
